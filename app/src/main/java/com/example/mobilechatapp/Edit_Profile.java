d67b22384b386507da5ddfa5dc4bc0b75fb8307f
Skidz0r <rui_hh@outlook.com>
2021-03-06 14:00:46 +0000

    Interactive way to change name + Change background color with a button.

--- app/src/main/java/com/example/mobilechatapp/Edit_Profile.java
+++ app/src/main/java/com/example/mobilechatapp/Edit_Profile.java
@@ -4,6 +4,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,7 @@ public class Edit_Profile extends AppCompatActivity {
     
     View screenView;
     Button clickMe;
+    Button clickChangeName;
     int[] color;
 
     @Override
@@ -40,5 +43,18 @@ public class Edit_Profile extends AppCompatActivity {
             }
         });
 
+        clickChangeName= (Button) findViewById(R.id.Change_Name_Button);
+        clickChangeName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText t = findViewById(R.id.My_Profile_Name);
+                String input = t.getText().toString();
+                ((TextView) findViewById(R.id.Profile_Name)).setText(input);
+                // Make a log
+                //Log.d(tag:"info", input);
+            }
+        });
+
     }
+
 }
