f0e1e15a3939dd0d787b9c378a9b13f1fb581d0d
Skidz0r <61596013+Skidz0r@users.noreply.github.com>
2021-05-30 16:52:21 +0100

    FrontScreen upgrade.

--- app/src/main/java/com/example/mobilechatapp/MainActivity.java
+++ app/src/main/java/com/example/mobilechatapp/MainActivity.java
@@ -15,38 +15,8 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            Button btConnect = (Button)findViewById(R.id.bluetoothSettingsButton);
-            Button profile = (Button)findViewById(R.id.profileButton);
-            Button chat = (Button)findViewById(R.id.chatButton);
             Button register = findViewById(R.id.registerButton);
             Button login = findViewById(R.id.loginButton);
-
-
-        btConnect.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View v)
-                {
-                    Intent btConnectIntent = new Intent(getApplicationContext(), BluetoothSetting.class);
-                    startActivity(btConnectIntent);
-                }
-            });
-
-            profile.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent profile_button = new Intent(getApplicationContext(), Profile.class);
-                    startActivity(profile_button);
-                }
-            });
-
-            chat.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent chatIntent = new Intent(getApplicationContext(), BluetoothChat.class);
-                    startActivity(chatIntent);
-                }
-            });
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
